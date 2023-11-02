c7032b8b9708683172590ea5e3a87dee22769805
Muhamad Amir <m.amirazmi@hotmail.com>
2023-11-02 15:01:01 +0800

    Add PM2 environment configs

---
+++ pm2/ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "burger-api",
+      cwd: "~/app/burger/api",
+      script: "npm",
+      args: "start",
+    },
+    {
+      name: "node-test-api",
+      cwd: "~/app/node-test",
+      script: "npm",
+      args: "start",
+    },
+  ],
+};
